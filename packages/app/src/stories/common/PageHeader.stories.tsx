e1dfb8c6b14045a94bff588d8cbcfe34fae1e5ef
danylokaba <avissuperbus@gmail.com>
2023-06-09 14:09:28 +0300

    feat: add mock redux state for PageHeader stories

--- packages/app/src/stories/common/PageHeader.stories.tsx
+++ packages/app/src/stories/common/PageHeader.stories.tsx
@@ -1,11 +1,60 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Provider } from 'react-redux';
-import { store } from 'store';
 
 import PageHeader from '@modules/common/PageHeader';
 
+const MockedState = {
+  addedRecord: {
+    message: null,
+    open: false,
+    type: 'success',
+  },
+};
+
+const MockedStateSuccess = {
+  addedRecord: {
+    message: 'Successfuly created',
+    open: true,
+    type: 'success',
+  },
+};
+
+const Mockstore = ({
+  children,
+  recordState,
+}: {
+  children: ReactNode;
+  recordState: any;
+}) => (
+  <Provider
+    store={configureStore({
+      reducer: {
+        design: combineReducers({
+          modals: createSlice({
+            name: 'taskbox',
+            initialState: recordState,
+            reducers: {},
+          }).reducer,
+        }),
+      },
+    })}
+  >
+    {children}
+  </Provider>
+);
+
+import { Button } from '@mui/material';
+import { combineReducers, configureStore, createSlice } from '@reduxjs/toolkit';
+import { ReactNode } from 'react';
+
+import { PageTitle } from '@modules/common/styled-mui';
+
 const withProvider = (story: any) => (
-  <Provider store={store}>{story()}</Provider>
+  <Mockstore recordState={MockedState}>{story()}</Mockstore>
+);
+
+const withProvideRecord = (story: any) => (
+  <Mockstore recordState={MockedStateSuccess}>{story()}</Mockstore>
 );
 
 const meta: Meta<typeof PageHeader> = {
@@ -18,6 +67,40 @@ const meta: Meta<typeof PageHeader> = {
 
 type Story = StoryObj<typeof PageHeader>;
 
-export const Default = <PageHeader>Title</PageHeader>;
+export const OnlyTitle: Story = {
+  decorators: [withProvider],
+  args: {
+    children: (
+      <>
+        <PageTitle>PageTitle</PageTitle>
+      </>
+    ),
+  },
+};
+
+export const WithButton: Story = {
+  decorators: [withProvider],
+  args: {
+    children: (
+      <>
+        <PageTitle>PageTitle</PageTitle>
+        <Button variant="contained">Add new</Button>
+      </>
+    ),
+  },
+};
+
+export const withRecord: Story = {
+  decorators: [withProvideRecord],
+  args: {
+    withRecord: true,
+    children: (
+      <>
+        <PageTitle>PageTitle</PageTitle>
+        <Button variant="contained">Add new</Button>
+      </>
+    ),
+  },
+};
 
 export default meta;
