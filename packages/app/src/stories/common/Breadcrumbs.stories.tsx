01a1bacc8294324c538e2f4c69bb6343419968fa
danylokaba <avissuperbus@gmail.com>
2023-06-12 17:00:07 +0300

    fix: add component description to Breadcrumbs stories

--- packages/app/src/stories/common/Breadcrumbs.stories.tsx
+++ packages/app/src/stories/common/Breadcrumbs.stories.tsx
@@ -11,6 +11,14 @@ const meta: Meta<typeof BreadCrumbs> = {
       description: 'The breadcrumbs is array of JSX.Element like Link',
     },
   },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'The Breadcrumbs component is a flexible component that allows you to create a visual representation of hierarchical navigation paths within your application. It takes an array of breadcrumb items and renders them in a visually pleasing and user-friendly manner. The Breadcrumbs component is particularly useful when you want to provide users with an easy way to navigate back or traverse through different levels of a website or application. Each breadcrumb item represents a specific page or section, allowing users to understand their current location within the overall structure.',
+      },
+    },
+  },
 };
 
 type Story = StoryObj<typeof BreadCrumbs>;
