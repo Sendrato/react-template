ba47f02fdbf270847762274b46bf790e04d6ce8e
danylokaba <avissuperbus@gmail.com>
2023-06-12 16:59:55 +0300

    fix: add component description to BasicLoader stories

--- packages/app/src/stories/common/BasicLoader.stories.tsx
+++ packages/app/src/stories/common/BasicLoader.stories.tsx
@@ -11,6 +11,14 @@ const meta: Meta<typeof BasicLoader> = {
       description: 'The loader size.',
     },
   },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'The BasicLoader component is a versatile and reusable component that provides a simple and consistent way to display loading indicators within your application. It offers a straightforward solution for indicating ongoing processes or waiting periods to users. The BasicLoader component can be easily integrated into any part of your application where loading feedback is required. Whether it`s fetching data from an API, performing calculations, or any other asynchronous operations.',
+      },
+    },
+  },
 };
 
 type Story = StoryObj<typeof BasicLoader>;
