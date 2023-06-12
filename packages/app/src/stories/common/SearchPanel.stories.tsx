334480117c7511a645c24c5a451e59742ee3b8d6
danylokaba <avissuperbus@gmail.com>
2023-06-12 17:03:47 +0300

    fix: add component description to SearchPanel stories

--- packages/app/src/stories/common/SearchPanel.stories.tsx
+++ packages/app/src/stories/common/SearchPanel.stories.tsx
@@ -20,6 +20,14 @@ const meta: Meta<typeof SearchPanel> = {
       description: 'The current value.',
     },
   },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'The SearchPanel component is a versatile and reusable component that provides a common user interface element for search functionality. It presents a search input field along with other components that can be customized to suit the overall design and requirements of your project.',
+      },
+    },
+  },
 };
 
 type Story = StoryObj<typeof SearchPanel>;
