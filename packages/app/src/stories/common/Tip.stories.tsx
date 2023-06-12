62b33df481da3abefa603eb72c841822af22e5a9
danylokaba <avissuperbus@gmail.com>
2023-06-12 17:20:17 +0300

    fix: add component description to Tip stories

--- packages/app/src/stories/common/Tip.stories.tsx
+++ packages/app/src/stories/common/Tip.stories.tsx
@@ -20,6 +20,14 @@ const meta: Meta<typeof Tip> = {
       description: 'The function by click on the button.',
     },
   },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'The Tip component offers different styles for displaying messages, including informational, success, and error styles. It can be easily integrated into any part of your application where you need to provide visual feedback or important information to users.',
+      },
+    },
+  },
 };
 
 type Story = StoryObj<typeof Tip>;
