b4461154d0ea15bc4aab2d083c9304c4900189ab
danylokaba <avissuperbus@gmail.com>
2023-06-14 17:10:11 +0300

    feat: add DatePicker stories

---
+++ packages/app/src/stories/common/DatePicker.stories.tsx
@@ -0,0 +1,58 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { useState } from 'react';
+
+import DatePicker from '@modules/common/DatePicker';
+
+const meta: Meta<typeof DatePicker> = {
+  title: 'Common/DatePicker',
+  component: DatePicker,
+  tags: ['autodocs'],
+  argTypes: {
+    minDate: {
+      description:
+        'The object with value, label, and error for input with min date.',
+    },
+    maxDate: {
+      description:
+        'The object with value, label, and error for input with max date.',
+    },
+    handleChangeMaxDate: {
+      description: 'Function for change max date',
+    },
+    handleChangeMinDate: {
+      description: 'Function for change min date',
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'The DatePicker component is a user-friendly and customizable component that enables users to select dates from a calendar-style interface. It provides an intuitive way to capture and display date inputs within your application.',
+      },
+    },
+  },
+};
+
+const Template: StoryFn<typeof DatePicker> = () => {
+  const [minValue, setMinValue] = useState('');
+  const [maxValue, setMaxValue] = useState('');
+
+  return (
+    <DatePicker
+      minDate={{
+        value: minValue,
+        label: 'From',
+      }}
+      handleChangeMinDate={setMinValue}
+      maxDate={{
+        value: maxValue,
+        label: 'To',
+      }}
+      handleChangeMaxDate={setMaxValue}
+    />
+  );
+};
+
+export const Default = Template.bind({});
+
+export default meta;
