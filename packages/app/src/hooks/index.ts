6b510aad42c86824a73a21ebcb5d90c5a423c9cf
danylokaba <avissuperbus@gmail.com>
2023-06-15 19:30:19 +0300

    feat: add use-locked-body hook

--- packages/app/src/hooks/index.ts
+++ packages/app/src/hooks/index.ts
@@ -1,6 +1,9 @@
 export { default as useCopyToClipboard } from './use-copy-to-clipboard';
+export { default as useEventListener } from './use-event-listener';
 export { default as useHistory } from './use-history';
+export { default as useInterval } from './use-interval';
 export { default as useKey } from './use-key';
+export { default as useLockedBody } from './use-locked-body';
 export { default as useMedia } from './use-media';
 export { default as usePagination } from './use-pagination';
 export { default as usePaginationState } from './use-pagination-state';
@@ -8,4 +11,5 @@ export { default as useScroll } from './use-scroll';
 export { default as useSelectedRow } from './use-selected-row';
 export { default as useSort } from './use-sort';
 export { default as useSortState } from './use-sort-state';
+export { default as useTimeout } from './use-timeout';
 export { default as useUpdateSession } from './use-update-session';
