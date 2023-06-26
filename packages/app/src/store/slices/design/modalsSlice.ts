06bc482c8a81d75ce8dfc092dcfe3f0fcfaab7bf
danylokaba <avissuperbus@gmail.com>
2023-06-26 15:30:11 +0300

    fix: delete redundant code from modal slice

--- packages/app/src/store/slices/design/modalsSlice.ts
+++ packages/app/src/store/slices/design/modalsSlice.ts
@@ -17,27 +17,17 @@ export const openAutoCloseRecord = createAsyncThunk<
 });
 
 type InitialStateType = {
-  openCreateSeller: boolean;
   addedRecord: {
     message: string | null;
     open: boolean;
     type: AlertType;
   };
-  sellerDetailsInfo: {
-    message: string | null;
-    open: boolean;
-  };
 };
 const initialState = {
-  openCreateSeller: false,
   addedRecord: {
     message: null,
     open: false,
     type: 'success',
-  },
-  sellerDetailsInfo: {
-    message: null,
-    open: false,
   },
 } as InitialStateType;
 
@@ -46,37 +36,21 @@ export const getModalsStore = createSelector(
   (substate: InitialStateType) => substate,
 );
 
-export const getIsOpenCreateSeller = createSelector(
-  getModalsStore,
-  (substate: InitialStateType) => substate.openCreateSeller,
-);
-
 export const getAddedRecord = createSelector(
   getModalsStore,
   (substate: InitialStateType) => substate.addedRecord,
-);
-
-export const getSellerDetailsInfo = createSelector(
-  getModalsStore,
-  (substate: InitialStateType) => substate.sellerDetailsInfo,
 );
 
 export const modalsSlice = createSlice({
   name: 'modalsSlice',
   initialState,
   reducers: {
-    setOpenCreateSeller(state, action) {
-      state.openCreateSeller = action.payload;
-    },
     setAddedRecord(state, action) {
       state.addedRecord = action.payload;
-    },
-    setSellerDetailsInfo(state, action) {
-      state.sellerDetailsInfo = action.payload;
     },
   },
 });
 
-export const { setOpenCreateSeller, setAddedRecord, setSellerDetailsInfo } = modalsSlice.actions;
+export const { setAddedRecord } = modalsSlice.actions;
 
 export default modalsSlice.reducer;
