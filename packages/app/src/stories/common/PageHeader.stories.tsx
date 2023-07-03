a61b0f405de960d31ad8698f6ad0e15506f7234e
danylokaba <avissuperbus@gmail.com>
2023-07-03 18:18:30 +0300

    fix: apply notificationDecorator to PageHeader story

--- packages/app/src/stories/common/PageHeader.stories.tsx
+++ packages/app/src/stories/common/PageHeader.stories.tsx
@@ -1,9 +1,23 @@
 /* eslint-disable react/display-name */
+import { TRecord } from '@interfaces/UI/notification';
 import { Button } from '@mui/material';
 import { Meta, StoryFn } from '@storybook/react';
+import { notificationsDecorator } from 'stories/decorators';
 
 import PageHeader from '@components/common/PageHeader';
 import { PageTitle } from '@components/styled-mui';
+
+const successRecord = {
+  record: { message: 'Success!', open: true, type: 'success' } as TRecord,
+};
+
+const errorRecord = {
+  record: { message: 'Something went wrong!', open: true, type: 'error' } as TRecord,
+};
+
+const infoRecord = {
+  record: { message: 'You have new message!', open: true, type: 'info' } as TRecord,
+};
 
 const meta: Meta<typeof PageHeader> = {
   title: 'Common/PageHeader',
@@ -38,8 +52,25 @@ const Template: StoryFn<typeof PageHeader> = (args) => {
 
 export const Default = Template.bind({});
 
-Default.bind({
+export const withSuccessRecord = Template.bind({});
+
+withSuccessRecord.decorators = [notificationsDecorator(successRecord)];
+withSuccessRecord.args = {
   withRecord: true,
-});
+};
+
+export const withErrorRecord = Template.bind({});
+
+withErrorRecord.decorators = [notificationsDecorator(errorRecord)];
+withErrorRecord.args = {
+  withRecord: true,
+};
+
+export const withInfoRecord = Template.bind({});
+
+withInfoRecord.decorators = [notificationsDecorator(infoRecord)];
+withInfoRecord.args = {
+  withRecord: true,
+};
 
 export default meta;
